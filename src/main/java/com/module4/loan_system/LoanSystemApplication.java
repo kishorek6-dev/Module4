55a288b83c413d1610a59ba8ee489a0e958ac69e
2478671 <2478671@cognizant.com>
2026-03-13 10:43:17 +0530

    New Commit

--- src/main/java/com/module4/loan_system/LoanSystemApplication.java
+++ src/main/java/com/module4/loan_system/LoanSystemApplication.java
@@ -9,6 +9,7 @@ public class LoanSystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LoanSystemApplication.class, args);
         System.out.println("Hello world");
+        System.out.println("Bye world");
 	}
 
 }
